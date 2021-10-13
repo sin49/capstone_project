f919286209f50c2e51be396dc07febfe1a579646
CoadCotake <aswwdfgh2580@naver.com>
2021-10-13 13:54:39 +0900

    스킬 쿨타임 합친것

--- capstone_project/Assets/Script/Skill.cs
+++ capstone_project/Assets/Script/Skill.cs
@@ -4,54 +4,65 @@ using UnityEngine;
 
 public class Skill : MonoBehaviour
 {
-    public skill_class[] s_c;
+    public skill_class[] s_c = new skill_class[2];
     public bool skill_slot_change;
     void Start()
     {
-        s_c[0]=new skill_class("테스트1", 1.0f);
+
+        s_c[0] = new skill_class("테스트1", 1.0f);
         s_c[1] = new skill_class("테스트2", 5.0f);
+        skill_slot_change = true;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        skill_system();
     }
     void skill_system()
     {
-        s_c[0].skill_cool();
-        s_c[1].skill_cool();
+
         if (skill_slot_change)
         {
-            if (Input.GetButtonDown("f"))
+            if (Input.GetKeyDown(KeyCode.F) && s_c[1].skill_available)
             {
+                Debug.Log("작동중입니다. F사용 ");
                 s_c[1].skill_active();
+                StartCoroutine(s_c[1].CoolTIme());
             }
-            if (Input.GetButtonDown("g"))
+            if (Input.GetKeyDown(KeyCode.G) && s_c[0].skill_available)
             {
+                Debug.Log("작동중입니다. G사용 ");
                 s_c[0].skill_active();
+                StartCoroutine(s_c[0].CoolTIme());
             }
         }
         else
         {
-            if (Input.GetButtonDown("f"))
+            if (Input.GetKeyDown(KeyCode.F) && s_c[0].skill_available)
             {
+                Debug.Log("작동중입니다. F사용2 ");
                 s_c[0].skill_active();
+                StartCoroutine(s_c[0].CoolTIme());
             }
-            if (Input.GetButtonDown("g"))
+            if (Input.GetKeyDown(KeyCode.G) && s_c[1].skill_available)
             {
+                Debug.Log("작동중입니다. G사용2 ");
                 s_c[1].skill_active();
+                StartCoroutine(s_c[1].CoolTIme());
             }
         }
-        if (Input.GetButtonDown("Tab"))
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
-            if(!skill_slot_change)
-                skill_slot_change =true;
+            Debug.Log("위치변환 ");
+            if (!skill_slot_change)
+                skill_slot_change = true;
             else
                 skill_slot_change = false;
         }
     }
 }
+
 public class skill_class
 {
     public string skill_name;
@@ -61,32 +72,46 @@ public class skill_class
     public float skill_timer;
     public bool skill_available;
     public GameObject skill_effect;//임시? 어떤 방식으로?
-    public skill_class(string c,float t){
+
+    public skill_class(string c, float t)
+    {
         skill_name = c;
         skill_cooltime = t;
         skill_available = true;
-        }
-    public void skill_active()//어떻게 접근할까? find?
-    {
-        if (skill_available == true)
-        {
-            Debug.Log(skill_name);
-            skill_available =false;
-        }
-       
     }
 
-    public void skill_cool()
+    public void skill_active()
     {
-        if (skill_available == false)
+        if (skill_available == true)    //작동
         {
-            skill_timer += Time.deltaTime;
-            if (skill_timer >= skill_cooltime)
-            {
-                skill_available = true;
-                skill_timer = 0;
-                Debug.Log(skill_name + "사용가능");
-            }
+            Debug.Log("작동합니다 스킬작동");
+            //skill_available =false;
+            skill_available = false;
         }
+    }
+
+    //public void skill_cool()
+    //{
+    //    skill_timer += Time.deltaTime;
+    //    if (skill_timer >= skill_cooltime)
+    //    {
+    //        skill_available = true;
+    //        skill_timer = 0;
+    //        Debug.Log(skill_name + "사용가능");
+    //    }
+    //}
+
+    public IEnumerator CoolTIme()
+    {
+        var wait = new WaitForSeconds(1f);
+
+        for (float i = skill_cooltime; i >= 0; i--)
+        {
+            yield return wait;
+        }
+        Debug.Log(skill_name + "쿨타임끝");
+        skill_available = true;
+        yield return null; //
+
     }
 }
