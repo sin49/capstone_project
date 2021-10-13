85b6d920b5bd227d3f5ca98abd235e1b33a4976e
CoadCotake <aswwdfgh2580@naver.com>
2021-10-13 16:13:03 +0900

    카메라 이동 구현

--- capstone_project/Assets/Script/MousePos.cs
+++ capstone_project/Assets/Script/MousePos.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MousePos : MonoBehaviour
 {
 
-    private void FixedUpdate()
+    private void Update()
     {
         this.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, -1);
     }
